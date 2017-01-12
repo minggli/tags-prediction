3c0fe1a088c9aa9c7d5ab5bded3f6007756c8e32
Ming <ming.george.li@gmail.com>
2017-01-12 12:53:53 +0000

    new commit

--- utils.py
+++ utils.py
@@ -5,8 +5,13 @@ from settings import PATHS
 
 zip_files = [f for f in os.listdir(PATHS['DATA']) if f.endswith('.zip')]
 
+df_objs = list()
+
 for i in zip_files:
     zip_obj = zf.ZipFile(file=PATHS['DATA'] + i)
     filename = zip_obj.namelist()[0]
     df = pd.read_csv(filepath_or_buffer=zip_obj.open(filename), index_col='id')
+    df_objs.append(df)
 
+
+print(df_objs)
