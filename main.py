ca48d08f7c1acdd50a04b8eb14c7c18680b6bfeb
Ming <ming.george.li@gmail.com>
2017-01-15 22:14:37 +0000

    new commit

--- main.py
+++ main.py
@@ -20,3 +20,19 @@ test_string = 'James is travelling to London this Sunday. We are too.'
 
 doc = nlp(test_string)
 
+print(doc)
+
+
+for sentence in doc.sents:
+    print(sentence)
+
+for token in doc:
+    print(token, token.lemma, token.lemma_, token.pos, token.pos_)
+
+for ent in doc.ents:
+    print(ent, ent.label, ent.label_)
+
+for np in doc.noun_chunks:
+    print(np)
+
+print(doc[0].similarity(doc[6]))
