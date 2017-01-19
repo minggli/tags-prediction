6a054317d22de36fb3420e343a3958d818b67b0d
Ming <ming.george.li@gmail.com>
2017-01-19 16:20:07 +0000

    new commit

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from settings import PATHS, PUNC
 import pandas as pd
 import numpy as np
 import spacy
-from bs4 import BeautifulSoup
-import string
-import re
 
 
 __author__ = 'Ming Li'
@@ -22,7 +19,6 @@ for k, i in enumerate(load):
         break
     print(k, i)
 
-
 # TODO find out how to predict tags
 
 sample = [final[i] for i in range(len(final))]
@@ -30,3 +26,6 @@ sample = [final[i] for i in range(len(final))]
 doc = nlp(str(sample[2]))
 
 print(doc)
+
+for token in doc:
+    print(token.pos_, token.pos)
