34a15ccd9b2a4ecd4ad624d94256f12eb0cc2d48
Ming <ming.george.li@gmail.com>
2017-03-03 22:27:45 +0800

    changing algo

--- engine.py
+++ engine.py
@@ -41,9 +41,27 @@ vectorized_train = list(train_tfidf)
 cv = CountVectorizer()
 cv.fit(vectorized_train)
 
+estimator = RandomForestClassifier(
+	n_estimators=10,
+	criterion='gini',
+	max_depth=None,
+	min_samples_split=2,
+	min_samples_leaf=1,
+	min_weight_fraction_leaf=0.0,
+	max_features='auto',
+	max_leaf_nodes=None,
+	min_impurity_split=1e-07,
+	bootstrap=True,
+	oob_score=False,
+	random_state=None,
+	verbose=0,
+	warm_start=False,
+	class_weight=None
+	)
+
 vectorized_train = cv.transform(vectorized_train)
 print('beginning training Multinomial Classifier with multi-label strategy...', end='', flush=True)
-OvR = OneVsRestClassifier(RandomForestClassifier(), n_jobs=-1)
+OvR = OneVsRestClassifier(estimator, n_jobs=-1)
 OvR.fit(vectorized_train, onehot_encoded_labels)
 print('done', flush=True)
 
